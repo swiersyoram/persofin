d159a71288cc7b0d1523c0892d47e50e10831678
Yoram Swiers <yoram@swiers.be>
2024-11-09 23:16:15 +0100

    update gitignore

---
+++ application/libs/utils/src/route.ts
@@ -0,0 +1,9 @@
+export enum ApplicationRoutes {
+  dashboard = '/dashboard',
+  accounts = '/dashboard/accounts',
+}
+
+export enum ApiRoutes {
+  accounts = '/api/accounts',
+  logout = '/api/auth/logout',
+}
