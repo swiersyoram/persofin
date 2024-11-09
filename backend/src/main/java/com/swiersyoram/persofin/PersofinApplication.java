9d8301b166ba3a2607186ecb082079bfe2a4b01f
Yoram Swiers <yoram@swiers.be>
2024-11-09 18:34:39 +0100

    update application

--- backend/src/main/java/com/swiersyoram/persofin/PersofinApplication.java
+++ backend/src/main/java/com/swiersyoram/persofin/PersofinApplication.java
@@ -2,8 +2,6 @@ package com.swiersyoram.persofin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @SpringBootApplication
 public class PersofinApplication {
@@ -11,15 +9,6 @@ public class PersofinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersofinApplication.class, args);
 	}
-
-
 }
 
-class MainController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from the main classes";
-    }
 
-
-}
