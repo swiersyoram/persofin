a261d34babcba20aca67e3b1265c97aa10bef59a
Yoram Swiers <yoram@swiers.be>
2024-11-09 22:53:32 +0100

    rename frontend to application

--- frontend/apps/web/src/app/somefile.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from "@persofin/shadcn"
-
-export const Test = () =>{
-  return (<Button>test</Button>)
-}
--- frontend/libs/web/components/shadcn/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Alert';
-export * from './AlertDialog';
-export * from './Button';
