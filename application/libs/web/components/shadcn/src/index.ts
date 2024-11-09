e3a38f2f5fe6ec304b20e1ef2f2da669ac94b621
Yoram Swiers <yoram@swiers.be>
2024-11-09 23:05:16 +0100

    move shadcn components

---
+++ application/libs/web/components/shadcn/src/index.ts
@@ -0,0 +1,7 @@
+export * from './Alert';
+export * from './AlertDialog';
+export * from './Button';
+export * from './Form';
+export * from './Label';
+export * from './Input';
+export * from './Card';
