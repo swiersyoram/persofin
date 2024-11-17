2bd5beb56084f346378d03e2c5087a2308170b4e
Yoram Swiers <yoram@swiers.be>
2024-11-17 17:20:38 +0100

    update api route proxy

--- application/apps/web/src/app/api/[...backend]/route.ts
+++ application/apps/web/src/app/api/[...backend]/route.ts
@@ -3,13 +3,18 @@ import { NextResponse } from 'next/server';
 import { API_URL } from '../config';
 
 export const GET = withApiAuthRequired(async (req) => {
-  const { accessToken } = await getAccessToken();
-  return NextResponse.json(
-    await fetch(API_URL + req.url.split('api')[1], {
-      headers: {
-        ...req.headers,
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }).then((res) => res.json())
-  );
+  try {
+    const res = new NextResponse();
+    const { accessToken } = await getAccessToken(req, res);
+    return NextResponse.json(
+      await fetch(API_URL + req.url.split('api')[1], {
+        headers: {
+          ...req.headers,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }).then((res) => res.json())
+    );
+  } catch (error) {
+    return NextResponse.json(error);
+  }
 });
