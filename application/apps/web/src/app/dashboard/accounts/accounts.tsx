adf6bbc231a6d84b4a61891ce9af462def49833a
Yoram Swiers <yoram@swiers.be>
2024-11-17 21:40:54 +0100

    accounts overview page

--- application/apps/web/src/app/dashboard/accounts/accounts.tsx
+++ application/apps/web/src/app/dashboard/accounts/accounts.tsx
@@ -1,9 +1,12 @@
 'use client';
 import React from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { Button } from '@persofin/shadcn';
 import { accountsQuery, QueryKeys } from '@persofin/api';
-import { AccountCard } from '@persofin/dashboard';
+import { AccountCard, DashboardTitle } from '@persofin/dashboard';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import Link from 'next/link';
+import { ApplicationRoutes } from '@persofin/utils';
 
 const Accounts = () => {
   const { data: accounts, ...props } = useQuery({
@@ -11,19 +14,22 @@ const Accounts = () => {
     queryFn: accountsQuery,
   });
 
-  const queryClient = useQueryClient();
-
   return (
-    <div>
-      <h1>Accounts</h1>
-      {accounts && <pre>{JSON.stringify(accounts)}</pre>}
-      {accounts?.map((account: { name: string; id: string }) => (
-        <AccountCard name={account.name} key={account.id} />
-      ))}
-
-      <Button onClick={() => queryClient.invalidateQueries()}>
-        invalidate
-      </Button>
+    <div className={'w-full'}>
+      <header className={'flex justify-between '}>
+        <DashboardTitle>Accounts</DashboardTitle>
+        <Link href={ApplicationRoutes.addAccount}>
+          <Button className={'space-x-2'}>
+            <AiOutlinePlusCircle fontSize={20} />
+            <span>Add account</span>
+          </Button>
+        </Link>
+      </header>
+      <div className={'mt-5 flex gap-4 flex-wrap h-fit'}>
+        {accounts?.map((account) => (
+          <AccountCard account={account} key={account.id} />
+        ))}
+      </div>
     </div>
   );
 };
