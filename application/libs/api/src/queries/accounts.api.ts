7f6f378707c1eb6e05a897a80670def9a44616ac
Yoram Swiers <yoram@swiers.be>
2024-11-17 21:41:22 +0100

    accounts endpoints

--- application/libs/api/src/queries/accounts.api.ts
+++ application/libs/api/src/queries/accounts.api.ts
@@ -1,6 +1,27 @@
 import { ApiRoutes } from '@persofin/utils';
+import { Account } from '@persofin/types';
 
-const baseQueryFunction = async (endpoint: string) =>
-  await fetch(endpoint).then((res) => res.json());
+const baseQueryFunction =
+  <T>(endpoint: string) =>
+  (): Promise<T> =>
+    fetch(endpoint).then((res) => res.json());
 
-export const accountsQuery = () => baseQueryFunction(ApiRoutes.accounts);
+const baseMutationFunction =
+  <T>(endpoint: string) =>
+  (data: T) => {
+    return fetch(endpoint, {
+      body: JSON.stringify(data),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+export const accountsQuery = baseQueryFunction<Array<Account>>(
+  ApiRoutes.accounts
+);
+
+export const addAccountMutation = baseMutationFunction<Account>(
+  ApiRoutes.accounts
+);
