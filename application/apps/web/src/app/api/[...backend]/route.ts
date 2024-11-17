e315f0d122dadbcff746f79de3977c8837a8786a
Yoram Swiers <yoram@swiers.be>
2024-11-17 21:10:02 +0100

    add post route in proxy

--- application/apps/web/src/app/api/[...backend]/route.ts
+++ application/apps/web/src/app/api/[...backend]/route.ts
@@ -4,17 +4,39 @@ import { API_URL } from '../config';
 
 export const GET = withApiAuthRequired(async (req) => {
   try {
-    const res = new NextResponse();
-    const { accessToken } = await getAccessToken(req, res);
-    return NextResponse.json(
-      await fetch(API_URL + req.url.split('api')[1], {
-        headers: {
-          ...req.headers,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }).then((res) => res.json())
-    );
+    const { accessToken } = await getAccessToken();
+    const response = await fetch(API_URL + req.url.split('api')[1], {
+      headers: {
+        ...req.headers,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return new NextResponse(await response.text(), {
+      status: response.status,
+    });
   } catch (error) {
-    return NextResponse.json(error);
+    return NextResponse.error();
+  }
+});
+
+export const POST = withApiAuthRequired(async (req, context) => {
+  try {
+    const { accessToken } = await getAccessToken();
+    const response = await fetch(API_URL + req.url.split('api')[1], {
+      headers: {
+        ...req.headers,
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(await req.json()),
+      method: 'POST',
+    });
+    const data = await response.text();
+    return new NextResponse(data, {
+      status: response.status,
+    });
+  } catch (error) {
+    return NextResponse.error();
   }
 });
